cb71e94e122fa9d1fd876bf2b644e07256c0f379
ayanda <64249187+ayandagatsha@users.noreply.github.com>
2021-08-17 12:00:19 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,98 @@
+import logo from './aya.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      {/* className="App-logo" */}
+        <img src={logo} className="App-aya" alt="logo" />
+        <h3>
+          Ayanda
+        </h3>
+        <h2>
+Javascript Web Applications Developer
+        </h2>
+
+        <h3>
+       User Experience Projects
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+        </h3>
+    
+      <h4>
+      <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        > Independent Videography Theatre Production    </a>  <span>(6 months)</span>
+        </h4>
+      
+     
+        <h4>
+      <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >     Shepard Training and Programming   </a>  <span>(3 years)</span>
+        </h4>
+   
+
+  
+       <h4>
+        User Tester and User Experience Administrator  <span>(5+ years)</span>
+        </h4>
+
+        <h5 className="paragra">
+Engagement facilitation: Catalyzing discourse between users in two different
+collections of our database. 
+        </h5>
+        <h5 className="paragra">
+Creating search string snippets to navigate databases for synonyms of a specific 
+type of user, engaging and recruiting them to a collection of our database. 
+        </h5>
+
+        <h5 className="paragra">
+Collaboration with C-level executives to confirm strategic marketing campaigns that utilize psyhological concepts. 
+        </h5>
+
+        <h5 className="paragra">
+Completion of programs awarded through earned scholarships   </h5>
+   
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+  <h4>
+
+  </h4>
+        </a>
+     
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+  
+        </a>
+
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+  
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
